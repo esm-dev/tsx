908ed914f8a42134e2fd29f002463ba67e9158b9
X <i@jex.me>
2023-11-30 00:16:24 +0800

    v0.3.3

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "0.3.2";
+export const VERSION = "0.3.3";
 
 /** `prepublish` will be invoked before publish */
 export async function prepublish(version: string): Promise<boolean> {
