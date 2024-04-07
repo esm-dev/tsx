ef3790c89f55b82fd9043f89597a3eff55fc2265
X <i@jex.me>
2024-04-07 10:41:29 +0800

    Refactor

--- js/swc.js
+++ js/swc.js
@@ -7,5 +7,12 @@ export function transform({ filename, code, ...options }) {
   if (typeof code !== "string") {
     throw new Error("code is required");
   }
+  if (options.importMap) {
+    if (typeof options.importMap === "object" && !Array.isArray(options.importMap)) {
+      options.importMap = JSON.stringify(options.importMap);
+    } else if (typeof options.importMap !== "string") {
+      throw new Error("importMap should be an object or a string");
+    }
+  }
   return swc(filename, code, options);
 }
--- version.js
+++ version.js
@@ -1,25 +1,20 @@
- /** `VERSION` managed by https://deno.land/x/publish */
+/** `VERSION` managed by https://deno.land/x/publish */
 export const VERSION = "0.5.4";
 
-/** `prepublish` will be invoked before publish */
 export async function prepublish(version) {
-  Deno.chdir(new URL(".", import.meta.url).pathname);
   const cargoToml = await Deno.readTextFile("./Cargo.toml");
-  const packageJson = await Deno.readTextFile("./package.json");
   await Deno.writeTextFile(
     "./Cargo.toml",
     cargoToml.replace(/version = "[\d\.]+"/, `version = "${version}"`),
-  );
-  await Deno.writeTextFile(
-    "./package.json",
-    packageJson.replace(/"version": "[\d\.]+"/, `"version": "${version}"`),
   );
   const ok = await run("wasm-pack", "build", "--target", "web");
   if (!ok) {
     return false;
   }
-  const { test } = await import("./js/test.js")
+
+  const { test } = await import("./js/test.js");
   await test();
+
   const dts = await Deno.readTextFile("./pkg/esm_compiler.d.ts");
   await Deno.writeTextFile(
     "./pkg/esm_compiler.d.ts",
@@ -35,22 +30,26 @@ export async function prepublish(version) {
       ),
     ].join("\n"),
   );
-  const wasmStat = await Deno.stat("./pkg/esm_compiler_bg.wasm");
-  console.log(
-    `wasm size: ${prettyBytes(wasmStat.size)}, gzipped: ${
-      prettyBytes(await getGzSize("./pkg/esm_compiler_bg.wasm"))
-    }`,
+
+  const packageJson = await Deno.readTextFile("./package.json");
+  await Deno.writeTextFile(
+    "./package.json",
+    packageJson.replace(/"version": "[\d\.]+"/, `"version": "${version}"`),
   );
+
+  const wasmStat = await Deno.stat("./pkg/esm_compiler_bg.wasm");
+  const prettyBytes = (n) => (n / 1024 / 1024).toFixed(2) + " MB";
+  console.log(
+    `wasm size: ${prettyBytes(wasmStat.size)}`,
+    `gzipped: ${prettyBytes(await getGzSize("./pkg/esm_compiler_bg.wasm"))}`,
+  );
+
   return true;
 }
 
-/** `postpublish` will be invoked after published */
+// publish to npmjs.org
 export async function postpublish(version) {
   await run("npm", "publish");
-}
-
-function prettyBytes(n) {
-  return (n / 1024 / 1024).toFixed(2) + " MB";
 }
 
 async function getGzSize(name) {
