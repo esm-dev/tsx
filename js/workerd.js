405d74d57c97a8513b0f78eac8d40f511d890adc
X <i@jex.me>
2024-04-07 10:41:17 +0800

    Export `WorkerEntrypoint` as default

--- js/workerd.js
+++ js/workerd.js
@@ -6,6 +6,33 @@ import indexHtml from "./index.html";
 const MB = 1024 * 1024;
 const errInvalidInput = new Error("invalid input");
 
+export default class EsmCompiler extends WorkerEntrypoint {
+  async fetch(req) {
+    if (req.method === "GET") {
+      return new Response(indexHtml, {
+        headers: { "content-type": "text/html; charset=utf-8" },
+      });
+    }
+    try {
+      const { filename, code, ...options } = validateInput(await req.json());
+      if (filename.endsWith(".css") || options.lang === "css") {
+        return Response.json(transformCSS(filename, code, options));
+      }
+      return Response.json(transform(filename, code, options));
+    } catch (err) {
+      return Response.json({ error: { message: err.message } }, { status: err.cause === errInvalidInput ? 400 : 500 });
+    }
+  }
+  async transform(input) {
+    const { filename, code, ...options } = validateInput(input);
+    return transform(filename, code, options);
+  }
+  async transformCSS(input) {
+    const { filename, code, ...options } = validateInput(input);
+    return transformCSS(filename, code, options);
+  }
+}
+
 function validateInput(input) {
   if (typeof input !== "object" || input === null) {
     throw new Error("input must be an object", { cause: errInvalidInput });
@@ -19,37 +46,14 @@ function validateInput(input) {
   if (input.code.length > 10 * MB) { // limit source code size to 10MB
     throw new Error("code is too large", { cause: errInvalidInput });
   }
+  if (input.importMap) {
+    if (typeof input.importMap === "object" && !Array.isArray(input.importMap)) {
+      input.importMap = JSON.stringify(input.importMap);
+    } else if (typeof input.importMap !== "string") {
+      throw new Error("importMap should be an object or a string", { cause: errInvalidInput });
+    }
+  }
   return input;
 }
-
-export class TransformService extends WorkerEntrypoint {
-  async transform(input) {
-    const { filename, code, ...options } = validateInput(input);
-    return transform(filename, code, options);
-  }
-  async transformCSS(input) {
-    const { filename, code, ...options } = validateInput(input);
-    return transformCSS(filename, code, options);
-  }
-}
-
-export default {
-  async fetch(req, env) {
-    if (req.method === "GET") {
-      return new Response(indexHtml, {
-        headers: { "content-type": "text/html" },
-      });
-    }
-    try {
-      const { filename, code, ...options } = validateInput(await req.json());
-      if (filename.endsWith(".css") || options.lang === "css") {
-        return Response.json(transformCSS(filename, code, options));
-      }
-      return Response.json(transform(filename, code, options));
-    } catch (err) {
-      return Response.json({ error: { message: err.message } }, { status: err.cause === errInvalidInput ? 400 : 500 });
-    }
-  },
-};
 
 initSync(wasm);
