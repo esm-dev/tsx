8e93dd78e5bc190d657c1f7cbc9b835cbbb61312
X <i@jex.me>
2024-06-20 10:35:45 +0800

    v0.6.0

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "0.5.7";
+export const VERSION = "0.6.0";
 
 export async function prepublish(version) {
   const cargoToml = await Deno.readTextFile("./Cargo.toml");
